e4525848fce58e136d5a1e0427b206d351c79320
Alfreds Genkins <alfreds@scandipwa.com>
2021-01-14 18:08:57 +0200

    with context (combination) implemented

--- packages/shopify/packages/@scandipwa/shopify-api/src/util/Context.js
+++ packages/shopify/packages/@scandipwa/shopify-api/src/util/Context.js
@@ -1,6 +1,18 @@
-// /** @namespace ShopifyApi/Context/WithApiClient/withApiClient */
-// export const withApiClient = (Component) => (props) => (
-//     <ApiClientContext.Consumer>
-//         { (consumerProps) => <Component props={ { ...props, consumerProps } } /> }
-//     </ApiClientContext.Consumer>
-// );
+/* eslint-disable @scandipwa/scandipwa-guidelines/jsx-no-props-destruction */
+
+/** @namespace ShopifyApi/Util/Context/withContexts */
+export const withContexts = (Component, contextOrContexts = []) => (props) => {
+    const contextArray = Array.isArray(contextOrContexts) ? contextOrContexts : [contextOrContexts];
+
+    const combinedConsumer = contextArray.reduce(
+        (acc, Context) => (value) => (
+            <Context.Consumer>
+                { /** merge them into one object (is that safe? */ }
+                { (contextValue) => acc({ ...value, ...contextValue }) }
+            </Context.Consumer>
+        ),
+        (value) => <Component { ...value } />
+    );
+
+    return combinedConsumer(props);
+};
