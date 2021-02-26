a9ed1ce3695304c02af5d834f8a48461bf318b45
Dmitrijs Asanovs <dmirijs.asanovs@scandiweb.com>
2021-02-26 17:50:44 +0200

    Docs for shopify-shop module

--- packages/shopify-shop/src/api/Shop.query.js
+++ packages/shopify-shop/src/api/Shop.query.js
@@ -1,7 +1,14 @@
 import { Field } from '@scandipwa/graphql';
 
-/** @namespace ShopifyShop/Api/Shop/Query/ShopQuery */
+/**
+ *  * Query declarations for shop
+ * @namespace ShopifyShop/Api/Shop/Query/ShopQuery */
 export class ShopQuery {
+    /**
+     * A function which returns an array of shop fields
+     * @extPoint Use it to add more fields for shop query (payment settings, privacy policy, etc.)
+     * @extExample (args, callback) => [...callback(...args), 'newField']
+     */
     _getShopFields() {
         return [
             'name',
--- packages/shopify-shop/src/context/Shop.provider.js
+++ packages/shopify-shop/src/context/Shop.provider.js
@@ -8,7 +8,9 @@ import CheckoutContext from './Shop.context';
 
 export const CHECKOUT_FROM_STORAGE = 'shopify_checkout';
 
-/** @namespace ShopifyShop/Context/Shop/Provider/ShopProvider */
+/**
+ * Provider class for Shop Context. Used to make shop data available to every component in the app.
+ * @namespace ShopifyShop/Context/Shop/Provider/ShopProvider */
 export class ShopProvider extends PureComponent {
     static contextType = ApiContext;
 
@@ -34,6 +36,14 @@ export class ShopProvider extends PureComponent {
         this.setState({ shop });
     }
 
+    /**
+     * A function which returns an object that will be visible in the Shop Context
+     * @extPoint Add more fields to Shop Context outside of Shopify API
+     * @extExample (args, callback) => ({
+     *      ...args,
+     *      supportedLanguages: ['EN', 'IT', 'ES']
+     * })
+     */
     getContextValue() {
         const { shop } = this.state;
         return { shop };
