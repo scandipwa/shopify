cf78497b68be777a116a70176dff6afe80a114aa
Dmitrijs Asanovs <dmirijs.asanovs@scandiweb.com>
2021-02-26 17:50:44 +0200

    Docs for shopify-shop module

---
+++ packages/shopify-shop/README.md
@@ -0,0 +1,11 @@
+# @scandipwa/shopify-shop
+
+This module implements the retrieval of Shopify shop information.
+
+## Declarations
+1. Allows to make shop query to the Shopify Storefront API.
+2. Declares Prop types for Shopify products.
+3. Adds Shop Context that shares the shop data to any component in the app.
+
+## Extensions
+1. Adds Shop Context provider to App component.
--- packages/shopify-shop/src/api/Shop.processor.js
+++ packages/shopify-shop/src/api/Shop.processor.js
@@ -1,2 +1,9 @@
-/** @namespace ShopifyShop/Api/Shop/Processor/processShop */
+/**
+ * Shop processor
+ * @extPoint Great place to add some "calculated" field into shop
+ * @extExample ([shop], callback) => {
+ *      callback(shop);
+ *      shop.translatedDescription = translate(shop.description);
+ * }
+ * @namespace ShopifyShop/Api/Shop/Processor/processShop */
 export const processShop = () => {};
