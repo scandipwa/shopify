3b303a1703e59c41d99e05dae8e479c0fa418a2e
Dmitrijs Asanovs <dmirijs.asanovs@scandiweb.com>
2021-02-26 15:08:14 +0200

    Documentation for shopify-collections-products

--- packages/shopify-collections-products/src/api/CollectionProducts.query.js
+++ packages/shopify-collections-products/src/api/CollectionProducts.query.js
@@ -2,9 +2,17 @@ import { Field } from '@scandipwa/graphql';
 import { mapQueryToType, TypedQuery } from '@scandipwa/shopify-api/src/util/TypedQuery';
 import getProductsQueryOfType, { PAGINATED_PRODUCTS } from '@scandipwa/shopify-products/src/api/Products.query';
 
+/**
+ * A type of `CollectionProductsQuery` associated with `getCollectionProducts` function.
+ * @example // Returns getter of single collection-products query
+ * import getCollectionProductsQueryByType, { SINGLE_PRODUCT_COLLECTION } from '%filename%';
+ * const queryGetter = getCollectionQueryByType(SINGLE_PRODUCT_COLLECTION);
+ */
 export const SINGLE_PRODUCT_COLLECTION = 'single';
 
-/** @namespace ShopifyCollectionsProducts/Api/CollectionProducts/Query/CollectionProductsQuery */
+/**
+ * Collection-products query declarations. This class is not intended to be used directly, instead prefer using "Typed Query" exported as default from this file.
+ * @namespace ShopifyCollectionsProducts/Api/CollectionProducts/Query/CollectionProductsQuery */
 export class CollectionProductsQuery extends TypedQuery {
     typeMap = {
         [SINGLE_PRODUCT_COLLECTION]: this.getCollectionProducts.bind(this)
