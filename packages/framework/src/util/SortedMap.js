a370f7ae8017b982567221caf1f70a29c2b4e423
Dmitrijs Asanovs <dmirijs.asanovs@scandiweb.com>
2021-03-03 13:44:58 +0200

    Remove placeholder code

--- packages/framework/src/util/SortedMap.js
+++ packages/framework/src/util/SortedMap.js
@@ -27,61 +27,6 @@ export class SortedList {
     };
 }
 
-/** @namespace Framework/Util/SortedMap/SortedRenderList2 */
-export class SortedRenderList2 {
-    __construct(array, options = {}) {
-        this.options = options;
-
-        this._unsortedMap = new Map(array.map(({ key, item }, i) => (
-            [key, { item, position: (i + 1) * DEFAULT_POSITION }]
-        )));
-    }
-
-    getSortedMap = () => {
-        const compareFunction = ([, a], [, b]) => a.position - b.position;
-
-        return new Map(
-            Array.from(this._unsortedMap.entries()).sort(compareFunction)
-        );
-    };
-
-    addItem = ({ item, position = this._getDefaultPosition(), key }) => {
-        if (!key) {
-            // eslint-disable-next-line max-len
-            console.warn('An item in SortedRenderList does not have a key specified. Setting the key is advised and this warning could lead to an unexpected behaviour otherwise.');
-        }
-
-        // Fallback for key is item object
-        this._unsortedMap.set(key || item, { item, position });
-    };
-
-    render = () => {
-        const { renderItem = this._renderItem } = this.options;
-
-        return Array.from(this.getSortedMap().entries(), ([key, { item }]) => renderItem(key, item));
-    };
-
-    getItemByKey = (key) => this._unsortedMap.get(key);
-
-    _renderItem(key, item) {
-        const child = item();
-
-        if (!isValidElement(child)) {
-            return (
-                <Fragment key={ key }>
-                    { child }
-                </Fragment>
-            );
-        }
-
-        return cloneElement(child, { key });
-    }
-
-    _getDefaultPosition() {
-        return (this._unsortedMap.size + 1) * DEFAULT_POSITION;
-    }
-}
-
 /** @namespace Framework/Util/SortedMap/SortedRenderList */
 export class SortedRenderList extends SortedList {
     _renderItem(item, key) {
@@ -103,9 +48,6 @@ export class SortedRenderList extends SortedList {
         return this.getSortedArray().map(renderItem);
     };
 }
-
-/** @namespace Framework/Util/SortedMap/createSortedRenderList2 */
-export const createSortedRenderList2 = (array = [], options) => new SortedRenderList2(array, options);
 
 /** @namespace Framework/Util/SortedMap/createSortedRenderList */
 export const createSortedRenderList = (array = [], options) => new SortedRenderList(array, options);
